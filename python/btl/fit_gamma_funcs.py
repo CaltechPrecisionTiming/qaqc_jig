0ed327098ca568dc909909afd785a80f953ae2d0
cptlab <cptlab@localhost.localdomain>
2023-07-27 12:08:40 -0700

    Measuring offset in gamma fits

--- python/btl/fit_gamma_funcs.py
+++ python/btl/fit_gamma_funcs.py
@@ -3,6 +3,8 @@ import numpy as np
 import sys
 import ROOT
 
+FIT_OPTIONS = 'LSB' 
+
 def ROOT_peaks(h, width=10, height=0.05, options=""):
     """
     Finds peaks in hisogram `h`. `height` is measured as a fraction of the
@@ -27,40 +29,48 @@ def ROOT_peaks(h, width=10, height=0.05, options=""):
     x_pos = x_pos[ind]
     return (x_pos, highest_peak)
 
-def fit_gamma(h, eng):
+def fit_gamma(h, hoffset, eng):
     """
-    Finds the tallest peak in the ROOT histogram `h`, and fits it with a
-    Gausssian. Computes picocoulombs per keV using the passed gamma energy
-    `eng`, and stores the result in parameter 0.  This procedure relies on
-    cutting crosstalk since otherwise the tallest peak will be around 0 pC.
-    """
-    win = 100
-    # FIXME: May want to tweak these peak finding parameters. Having `width=10`
-    # seems too small, but if it's made bigger, the peak-finding algorithm
-    # struggles to find the 511 keV peak. 
-    # `peak` is the charge of the tallest peak in the spectrum.
-    peak = ROOT_peaks(h, width=10, height=0.05, options="nobackground")[1]
+    Finds the full energy gamma peak (with energy `eng`) in the ROOT histogram
+    `h`, and fits it with a Gausssian. Returns the fit parameters, with the
+    first parameter being pC per keV.
     
-    # `ROOT_peaks` should find at least one peak, ideally the 511 keV peak.
-    if peak == None:
+    Also fits the peak corresponding to 0 keV events to measure any offset.
+    """
+    # First we find the offset peak.
+    offset = hoffset.GetBinCenter(hoffset.GetMaximumBin())
+    offset_pars = fit_offset(hoffset, offset)
+    if offset_pars is None:
         return None
-
+    offset = offset_pars[0][0]
+    offset_sigma = offset_pars[0][1]
+    
+    # Now we find the full energy peak. We don't use `GetMaximumBin` because we
+    # want our estimate of the full energy peak to be sufficiently far away
+    # from the offset peak.
+    peak_bin = None
+    for i in range(h.FindBin(offset + 3*offset_sigma), h.GetNbinsX()):
+        if peak_bin is None or h.GetBinContent(i) > h.GetBinContent(peak_bin):
+            peak_bin = i
+    if peak_bin is None:
+        return None
+    peak = h.GetBinCenter(peak_bin)
     # Gaussian + linear background
-    # f = ROOT.TF1(f"{h.GetName()}_fit",f"[2]*exp(-0.5*(x-[0]*{eng})**2/[1]**2) + [3]*x + [4]", 0, 1000)
-    f = ROOT.TF1(f"{h.GetName()}_fit",f"[2]*exp(-0.5*(x-[0]*{eng})**2/[1]**2)", 0, 1000)
-    fit_options = 'LSB' 
+    # f = ROOT.TF1(f"{h.GetName()}_fit",f"[2]*exp(-0.5*(x-{offset}-[0]*{eng})**2/[1]**2) + [3]*x + [4]", 0, 1000)
+    f = ROOT.TF1(f"{h.GetName()}_fit",f"[2]*exp(-0.5*(x-{offset}-[0]*{eng})**2/[1]**2)", 0, 1000)
+    sigma = 100
     f.SetParameter(0, peak/eng)
-    f.SetParameter(1, win/2)
+    f.SetParameter(1, sigma/2)
     f.SetParameter(2, h.GetBinContent(h.FindBin(peak)))
     
-    r = h.Fit(f, fit_options, '', peak-win, peak+win)
+    r = h.Fit(f, FIT_OPTIONS, '', peak-sigma, peak+sigma)
     h.Write()
     # Secondary fit that limits the range to +/-1.5 sigma. This makes the model
     # more accurate. The expression includes `abs` because sometimes ROOT makes
     # sigma negative. This is not ideal, but when I tried limiting sigma to
     # positive values, ROOT had trouble fitting some histograms for unknown
     # reasons.
-    r = h.Fit(f, fit_options, '', eng*f.GetParameter(0) - 1.5*abs(f.GetParameter(1)), eng*f.GetParameter(0) + 1.5*abs(f.GetParameter(1)))
+    r = h.Fit(f, FIT_OPTIONS, '', eng*f.GetParameter(0) - 1.5*abs(f.GetParameter(1)), eng*f.GetParameter(0) + 1.5*abs(f.GetParameter(1)))
     f.Write()
     h.Write()
     try:
@@ -72,4 +82,38 @@ def fit_gamma(h, eng):
     # adds 2 more parameters. 
     # return [f.GetParameter(i) for i in range(5)], [f.GetParError(i) for i in range(5)]
     return [f.GetParameter(i) for i in range(3)], [f.GetParError(i) for i in range(3)]
+
+def fit_offset(h, peak):
+    """
+    Fits the peak corresponding to 0 keV events to measure any offset.
+    
+    Returns the fit parameters.
+    """
+    sigma = 10
+    # FIXME: Gaussian noise model is just a guess. However, we really only need
+    # the average noise, which a narrow Gaussian can roughly estimate.
+    f = ROOT.TF1(f"{h.GetName()}_offset_fit", "gaus", -100, 100)
+    f.SetParameter(1, peak)
+    f.SetParameter(2, sigma)
+    f.SetParameter(0, h.GetBinContent(h.FindBin(peak)))
+    
+    r = h.Fit(f, FIT_OPTIONS, '', peak-sigma, peak+sigma)
+    h.Write()
+    # Secondary fit that we limit to +/-1.5 sigma. 
+    r = h.Fit(f, FIT_OPTIONS, '', f.GetParameter(1) - 1.5*abs(f.GetParameter(2)), f.GetParameter(1) + 1.5*abs(f.GetParameter(2)))
+    f.Write()
+    h.Write()
+
+    try:
+        if not r.Get().IsValid():
+            return None
+    except Exception as e:
+        return None
+    # 1: mean
+    # 2: sigma
+    # 0: scale
+    par_order = (1, 2, 0)
+    return [f.GetParameter(i) for i in par_order], [f.GetParError(i) for i in par_order]
+
+
 
